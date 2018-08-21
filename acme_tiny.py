0e1bb6e7ea41e7f24a460371ac176e6d097e214a
Richard Mansfield <richard@dragonfly.co.nz>
2018-08-21 12:02:35 +1200

    Version for dns challenges using AWS dns (route53)
    The setup_dns and delete_dns functions are taken from
    https://gist.github.com/astraw/553bd84659a7bb76bd3117c0f7c7af24

--- acme_tiny.py
+++ acme_tiny.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # Copyright Daniel Roesler, under MIT license, see LICENSE at github.com/diafygi/acme-tiny
 import argparse, subprocess, json, os, sys, base64, binascii, time, hashlib, re, copy, textwrap, logging
+import boto3
+
 try:
     from urllib.request import urlopen, Request # Python 3
 except ImportError:
@@ -13,7 +15,51 @@ LOGGER = logging.getLogger(__name__)
 LOGGER.addHandler(logging.StreamHandler())
 LOGGER.setLevel(logging.INFO)
 
-def get_crt(account_key, csr, acme_dir, log=LOGGER, CA=DEFAULT_CA, disable_check=False, directory_url=DEFAULT_DIRECTORY_URL, contact=None):
+def setup_dns(domain, txt_challenge, hosted_zone_id, aws_access_key_id, aws_secret_access_key):
+    session = boto3.Session()
+    client = session.client(
+        "route53", aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+    resp = client.change_resource_record_sets(
+        HostedZoneId=hosted_zone_id,
+        ChangeBatch={
+            'Changes': [{
+                'Action': 'UPSERT',
+                'ResourceRecordSet': {
+                    'Name': '_acme-challenge.{0}'.format(domain),
+                    'Type': 'TXT',
+                    'TTL': 60,
+                    'ResourceRecords': [{
+                        'Value': '"{0}"'.format(txt_challenge)
+                    }]
+                }
+            }]
+        }
+    )
+    # wait 30 seconds for DNS update
+    time.sleep(30)
+
+def delete_dns(domain, txt_challenge, hosted_zone_id, aws_access_key_id, aws_secret_access_key):
+    session = boto3.Session()
+    client = session.client(
+        "route53", aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+    resp = client.change_resource_record_sets(
+        HostedZoneId=hosted_zone_id,
+        ChangeBatch={
+            'Changes': [{
+                'Action': 'DELETE',
+                'ResourceRecordSet': {
+                    'Name': '_acme-challenge.{0}'.format(domain),
+                    'Type': 'TXT',
+                    'TTL': 60,
+                    'ResourceRecords': [{
+                        'Value': '"{0}"'.format(txt_challenge)
+                    }]
+                }
+            }]
+        }
+    )
+
+def get_crt(account_key, csr, aws_hosted_zone, aws_access_key_id, aws_secret_access_key, log=LOGGER, CA=DEFAULT_CA, disable_check=False, directory_url=DEFAULT_DIRECTORY_URL, contact=None):
     directory, acct_headers, alg, jwk = None, None, None, None # global variables
 
     # helper functions - base64 encode for jose spec
@@ -121,34 +167,28 @@ def get_crt(account_key, csr, acme_dir, log=LOGGER, CA=DEFAULT_CA, disable_check
     order, _, order_headers = _send_signed_request(directory['newOrder'], order_payload, "Error creating new order")
     log.info("Order created!")
 
+    records_to_clean = []
     # get the authorizations that need to be completed
     for auth_url in order['authorizations']:
         authorization, _, _ = _do_request(auth_url, err_msg="Error getting challenges")
         domain = authorization['identifier']['value']
         log.info("Verifying {0}...".format(domain))
 
-        # find the http-01 challenge and write the challenge file
-        challenge = [c for c in authorization['challenges'] if c['type'] == "http-01"][0]
+        # find the dns-01 challenge and write the challenge file
+        challenge = [c for c in authorization['challenges'] if c['type'] == "dns-01"][0]
         token = re.sub(r"[^A-Za-z0-9_\-]", "_", challenge['token'])
         keyauthorization = "{0}.{1}".format(token, thumbprint)
-        wellknown_path = os.path.join(acme_dir, token)
-        with open(wellknown_path, "w") as wellknown_file:
-            wellknown_file.write(keyauthorization)
-
-        # check that the file is in place
-        try:
-            wellknown_url = "http://{0}/.well-known/acme-challenge/{1}".format(domain, token)
-            assert(disable_check or _do_request(wellknown_url)[0] == keyauthorization)
-        except (AssertionError, ValueError) as e:
-            os.remove(wellknown_path)
-            raise ValueError("Wrote file to {0}, but couldn't download {1}: {2}".format(wellknown_path, wellknown_url, e))
+        keydigest64 = _b64(hashlib.sha256(keyauthorization.encode("utf8")).digest())
+        setup_dns(domain, keydigest64, aws_hosted_zone, aws_access_key_id, aws_secret_access_key)
 
         # say the challenge is done
         _send_signed_request(challenge['url'], {}, "Error submitting challenges: {0}".format(domain))
         authorization = _poll_until_not(auth_url, ["pending"], "Error checking challenge status for {0}".format(domain))
         if authorization['status'] != "valid":
+            delete_dns(domain, keydigest64, aws_hosted_zone, aws_access_key_id, aws_secret_access_key)
             raise ValueError("Challenge did not pass for {0}: {1}".format(domain, authorization))
         log.info("{0} verified!".format(domain))
+        records_to_clean.append((domain, keydigest64))
 
     # finalize the order with the csr
     log.info("Signing certificate...")
@@ -163,6 +203,8 @@ def get_crt(account_key, csr, acme_dir, log=LOGGER, CA=DEFAULT_CA, disable_check
     # download the certificate
     certificate_pem, _, _ = _do_request(order['certificate'], err_msg="Certificate download failed")
     log.info("Certificate signed!")
+    for domain, keydigest64 in records_to_clean:
+        delete_dns(domain, keydigest64, aws_hosted_zone, aws_access_key_id, aws_secret_access_key)
     return certificate_pem
 
 def main(argv=None):
@@ -182,7 +224,9 @@ def main(argv=None):
     )
     parser.add_argument("--account-key", required=True, help="path to your Let's Encrypt account private key")
     parser.add_argument("--csr", required=True, help="path to your certificate signing request")
-    parser.add_argument("--acme-dir", required=True, help="path to the .well-known/acme-challenge/ directory")
+    parser.add_argument("--aws-hosted-zone", required=True, help="AWS hosted zone id")
+    parser.add_argument("--aws-access-key-id", required=True, help="AWS access key id")
+    parser.add_argument("--aws-secret-access-key", required=True, help="AWS secret access key")
     parser.add_argument("--quiet", action="store_const", const=logging.ERROR, help="suppress output except for errors")
     parser.add_argument("--disable-check", default=False, action="store_true", help="disable checking if the challenge file is hosted correctly before telling the CA")
     parser.add_argument("--directory-url", default=DEFAULT_DIRECTORY_URL, help="certificate authority directory url, default is Let's Encrypt")
@@ -191,7 +235,8 @@ def main(argv=None):
 
     args = parser.parse_args(argv)
     LOGGER.setLevel(args.quiet or LOGGER.level)
-    signed_crt = get_crt(args.account_key, args.csr, args.acme_dir, log=LOGGER, CA=args.ca, disable_check=args.disable_check, directory_url=args.directory_url, contact=args.contact)
+    signed_crt = get_crt(args.account_key, args.csr, args.aws_hosted_zone, args.aws_access_key_id, args.aws_secret_access_key, log=LOGGER, CA=args.ca, disable_check=args.disable_check, directory_url=args.directory_url, contact=args.contact)
+
     sys.stdout.write(signed_crt)
 
 if __name__ == "__main__": # pragma: no cover
